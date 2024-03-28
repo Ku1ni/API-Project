84a5db71652bc0ac461cf954d32959b8bafe645d
jasmine <jazzy96db@gmail.com>
2024-03-28 13:32:03 -0500

    Add Users table migration and seeds

---
+++ backend/db/seeders/20240328182029-demo-user.js
@@ -0,0 +1,57 @@
+'use strict';
+const { User } = require('../models');
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+   try{
+   await User.bulkCreate([
+    {
+      email: 'demo@user.io',
+      username: 'Demo-lition',
+      hashedPassword: bcrypt.hashSync('password', 12)
+    },
+    {
+      email: 'user1@user.io',
+      username: 'FakeUser1',
+      hashedPassword: bcrypt.hashSync('password2', 10)
+    },
+    {
+      email: 'user2@user.io',
+      username: 'FakeUser2',
+      hashedPassword: bcrypt.hashSync('password3', 16)
+    }
+   ], { validate: true})
+  } catch (error){
+    console.error('Error occurred during password hashing:', error);
+  }
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
