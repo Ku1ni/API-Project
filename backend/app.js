54a186d2c6a8b39c6503ad834fc46b6605c4af74
jasmine <jazzy96db@gmail.com>
2024-03-28 12:49:13 -0500

    Add error-handling middleware for common errors and debugging

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const isProduction = environment === 'production';
@@ -15,18 +16,14 @@ app.use(morgan('dev'));
 
 app.use(cookieParser());
 app.use(express.json());
-// app.use(helmet({
-//     contentSecurityPolicy: {
-//       directives: {
-//         crossOriginResourcePolicy: { policy: 'cross-origin' }
-//       }
-//     }
-//   }));
+
 app.use(
     helmet.crossOriginResourcePolicy({
       policy: "cross-origin",
     })
   );
+
+
 
   // Security Middleware
 if (!isProduction) {
@@ -62,5 +59,39 @@ app.use(routes); // Connect all the routes
 // app.listen(PORT, () => {
 //   console.log(`Server is running on port ${PORT}`);
 // });
+  // Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = 'Validation error';
+    err.errors = errors;
+  }
+  next(err);
+});
+
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || 'Server Error',
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack
+  });
+});
 
 module.exports = app;
