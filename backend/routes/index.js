562ae44b30aa3189d489ccbf05bfa6376a980bb6
jasmine <jazzy96db@gmail.com>
2024-05-31 22:56:23 -0500

    Phase 0 Completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -5,32 +5,31 @@ const router = express.Router();
 const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
+if (process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  // Serve the frontend's index.html file at the root route
+  router.get('/', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+  router.get(/^(?!\/?api).*/, (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.sendFile(
+      path.resolve(__dirname, '../../frontend', 'dist', 'index.html')
+    );
+  });
+}
 
-// const express = require('express');
-// const router = express.Router();
-
-// router.get('/hello/world', function(req, res) {
-//   res.cookie('XSRF-TOKEN', req.csrfToken());
-//   res.send('Hello World!');
-// });
-
-// module.exports = router;
 
 // Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-    const csrfToken = req.csrfToken();
-    res.cookie("XSRF-TOKEN", csrfToken);
-    res.status(200).json({
-      'XSRF-Token': csrfToken
-    });
+if (process.env.NODE_ENV !== 'production') {
+  router.get('/api/csrf/restore', (req, res) => {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    return res.json({});
   });
-//   fetch('/api/test', {
-//     method: "POST",
-//     headers: {
-//       "Content-Type": "application/json",
-//       "XSRF-TOKEN": "WpVPxQxp-dKxSewBlHSNNLNJvw-_QaXLU_0o"
-//     },
-//     body: JSON.stringify({ hello: 'world' })
-//   }).then(res => res.json()).then(data => console.log(data));
+}
+
 
   module.exports = router;
